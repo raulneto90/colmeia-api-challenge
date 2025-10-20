d1b3ddc81858d16e530f703b34eced1528bec943
Raul Neto <raulneto90@gmail.com>
2025-10-20 20:22:44 -0300

    feat: update CustomersModule to include controllers and services for customer creation

--- src/modules/customers/customers.module.ts
+++ src/modules/customers/customers.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../common/services/prisma.service';
+import { CreateCustomerUseCase } from './application/create-customer/create-customer.usecase';
 import { PostgresCustomerRepository } from './infrastructure/repositories/postgres-customer.repository';
+import { CreateCustomerController } from './interfaces/create-customer/create-customer.controller';
 
 @Module({
 	imports: [],
-	controllers: [],
-	providers: [PostgresCustomerRepository],
+	controllers: [CreateCustomerController],
+	providers: [
+		PrismaService,
+		{
+			provide: 'CustomersRepository',
+			useClass: PostgresCustomerRepository,
+		},
+		CreateCustomerUseCase,
+	],
 })
 export class CustomersModule {}
